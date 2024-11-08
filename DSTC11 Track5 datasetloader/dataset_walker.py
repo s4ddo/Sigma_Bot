9fc9bd601104202f330dc17c5abf6448257f62bf
s4ddo <ahmadramadan407@gmail.com>
2024-11-08 14:00:37 +0100

    SigmaCommit

---
+++ DSTC11 Track5 datasetloader/dataset_walker.py
@@ -0,0 +1,83 @@
+import json
+import os
+
+from knowledge_reader import KnowledgeReader
+
+
+class DatasetWalker(object):
+    def __init__(self, dataset, dataroot, labels=False, labels_file=None, incl_knowledge=False):
+        path = os.path.join(os.path.abspath(dataroot))
+
+        if dataset not in ['train', 'val', 'test']:
+            raise ValueError('Wrong dataset name: %s' % (dataset))
+
+        logs_file = os.path.join(path, dataset, 'logs.json')
+        with open(logs_file, 'r') as f:
+            self.logs = json.load(f)
+
+        self.labels = None
+
+        if labels is True:
+            if labels_file is None:
+                labels_file = os.path.join(path, dataset, 'labels.json')
+
+            with open(labels_file, 'r') as f:
+                self.labels = json.load(f)
+
+        self._incl_knowledge = incl_knowledge
+        if self._incl_knowledge is True:
+            self._knowledge = KnowledgeReader(dataroot)
+
+    def __iter__(self):
+        if self.labels is not None:
+            for log, label in zip(self.logs, self.labels):
+                if self._incl_knowledge is True and label['target'] is True:
+                    label = self.resolve_knowledge(label)
+                yield (log, label)
+        else:
+            for log in self.logs:
+                yield (log, None)
+
+    def __len__(self, ):
+        return len(self.logs)
+
+    def __getitem__(self, item):
+        if self.labels is not None:
+            log = self.logs[item]
+            label = self.labels[item]
+            if self._incl_knowledge is True and label['target'] is True:
+                label = self.resolve_knowledge(label)
+            return (log, label)
+        else:
+            return (self.logs[item], None)
+
+    def resolve_knowledge(self, label):
+        for idx, snippet in enumerate(label['knowledge']):
+            domain = snippet['domain']
+            entity_id = snippet['entity_id']
+            doc_type = snippet['doc_type']
+            doc_id = snippet['doc_id']
+
+            if doc_type == 'review':
+                sent_id = snippet['sent_id']
+                sent = self._knowledge.get_review_sent(domain, entity_id, doc_id, sent_id)
+                label['knowledge'][idx]['sent'] = sent
+
+            elif doc_type == 'faq':
+                doc = self._knowledge.get_faq_doc(domain, entity_id, doc_id)
+                question = doc['question']
+                answer = doc['answer']
+
+                label['knowledge'][idx]['question'] = question
+                label['knowledge'][idx]['answer'] = answer
+
+        return label
+
+    def filter_knowledge_only(self):
+        x, y = [], []
+        for lab, log in zip(self.labels, self.logs):
+            if lab['target']:
+                x.append(lab)
+                y.append(log)
+
+        self.labels, self.logs = x, y
